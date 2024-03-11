d9df367e47262cfdca3e35e60d2bc15a86e77aa0
haygcao <ngokelcho@outlook.com>
2024-03-11 18:46:09 +0800

    Update contact.dart

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -1,1 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'contact.g.dart';
+
+@JsonSerializable()
+class Contact {
+  final String id;
+  final String name;
+  final String phoneNumber;
+  final String email;
+  final String remark;
+
+  Contact({
+    required this.id,
+    required this.name,
+    this.phoneNumber = '',
+    this.email = '',
+    this.remark = '',
+  });
+
+  factory Contact.fromJson(Map<String, dynamic> json) => _$ContactFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ContactToJson(this);
+}
 
