f91b47cf00665aae27cdddda00e027a164af2688
haygcao <ngokelcho@outlook.com>
2024-03-11 18:39:41 +0800

    Update label.dart

--- lib/models/label.dart
+++ lib/models/label.dart
@@ -1,1 +1,23 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'label.g.dart';
+
+@JsonSerializable()
+class Label {
+  final String id; // 标签 ID
+  final String name; // 标签名称
+  final String avatar; // 标签头像
+  final String label; // 标签文本内容
+
+  Label({
+    required this.id,
+    required this.name,
+    required this.avatar,
+    required this.label,
+  });
+
+  factory Label.fromJson(Map<String, dynamic> json) => _$LabelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LabelToJson(this);
+}
 
