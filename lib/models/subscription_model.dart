325336b599a419ff111faa843919259aca5625bd
haygcao <ngokelcho@outlook.com>
2024-03-12 00:12:58 +0800

    Update subscription_model.dart

--- lib/models/subscription_model.dart
+++ lib/models/subscription_model.dart
@@ -1,21 +1,81 @@
-// models/subscription_model.dart
+// models/subscription_entity.dart
 
 part of 'subscription_service.dart';
 
-@JsonSerializable()
-class Subscription {
-  final String id;
+import 'package:floor/floor.dart';
+
+// 定义数据库实体
+@entity
+class SubscriptionEntity {
+  @primaryKey
+  final int id;
+
   final String name;
-  final String url;
 
-  Subscription({
-    required this.id,
-    required this.name,
-    required this.url,
-  });
+  final String number;
 
-  factory Subscription.fromJson(Map<String, dynamic> json) => _$SubscriptionFromJson(json);
+  final bool enabled;
 
-  Map<String, dynamic> toJson() => _$SubscriptionToJson(this);
+  final bool isWhitelist;
+
+  final bool isBlacklist;
+
+  SubscriptionEntity(
+      this.id,
+      this.name,
+      this.number,
+      this.enabled,
+      this.isWhitelist,
+      this.isBlacklist);
 }
 
+// 定义数据库
+@database
+class AppDatabase {
+  // 定义数据库版本
+  static const int version = 100;
+
+  // 定义数据库中的表
+  @dao
+  SubscriptionEntityDao get subscriptionEntityDao;
+}
+
+// 定义数据访问对象
+@dao
+abstract class SubscriptionEntityDao {
+  // 插入订阅
+  @insert
+  Future<void> insertSubscriptionEntity(SubscriptionEntity subscriptionEntity);
+
+  // 查询所有订阅
+  @query
+  Future<List<SubscriptionEntity>> getAllSubscriptionEntities();
+
+  // 根据 ID 查询订阅
+  @query
+  Future<SubscriptionEntity> getSubscriptionEntityById(int id);
+
+  // 更新订阅
+  @update
+  Future<void> updateSubscriptionEntity(SubscriptionEntity subscriptionEntity);
+
+  // 删除订阅
+  @delete
+  Future<void> deleteSubscriptionEntity(SubscriptionEntity subscriptionEntity);
+
+  // 查询白名单订阅
+  @query
+  Future<List<SubscriptionEntity>> getWhitelistSubscriptionEntities();
+
+  // 查询黑名单订阅
+  @query
+  Future<List<SubscriptionEntity>> getBlacklistSubscriptionEntities();
+
+  // 根据号码查询订阅
+  @query
+  Future<SubscriptionEntity> getSubscriptionEntityByNumber(String number);
+
+  // 根据是否启用查询订阅
+  @query
+  Future<List<SubscriptionEntity>> getSubscriptionEntitiesByEnabled(bool enabled);
+}
