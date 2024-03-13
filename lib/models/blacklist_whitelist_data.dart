64a0b1d29c4ada3eea1e48aab5bf58c78c40b198
haygcao <ngokelcho@outlook.com>
2024-03-13 17:25:30 +0800

    Update and rename blacklist.dart to blacklist_whitelist_data.dart

---
+++ lib/models/blacklist_whitelist_data.dart
@@ -0,0 +1,72 @@
+class BlacklistEntry {
+  final int id;
+  final String avatar;
+  final String label;
+  final String phoneNumber;
+  final String name;
+  final bool isSubscribed;
+
+  BlacklistEntry({
+    required this.id,
+    required this.avatar,
+    required this.label,
+    required this.phoneNumber,
+    required this.name,
+    required this.isSubscribed,
+  });
+
+  factory BlacklistEntry.fromJson(Map<String, dynamic> json) => BlacklistEntry(
+        id: json['id'],
+        avatar: json['avatar'],
+        label: json['label'],
+        phoneNumber: json['phoneNumber'],
+        name: json['name'],
+        isSubscribed: json['isSubscribed'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'avatar': avatar,
+        'label': label,
+        'phoneNumber': phoneNumber,
+        'name': name,
+        'isSubscribed': isSubscribed,
+      };
+}
+
+class WhitelistEntry {
+  final int id;
+  final String avatar;
+  final String label;
+  final String phoneNumber;
+  final String name;
+  final bool isSubscribed;
+
+  WhitelistEntry({
+    required this.id,
+    required this.avatar,
+    required this.label,
+    required this.phoneNumber,
+    required this.name,
+    required this.isSubscribed,
+  });
+
+  factory WhitelistEntry.fromJson(Map<String, dynamic> json) => WhitelistEntry(
+        id: json['id'],
+        avatar: json['avatar'],
+        label: json['label'],
+        phoneNumber: json['phoneNumber'],
+        name: json['name'],
+        isSubscribed: json['isSubscribed'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'avatar': avatar,
+        'label': label,
+        'phoneNumber': phoneNumber,
+        'name': name,
+        'isSubscribed': isSubscribed,
+      };
+}
+
