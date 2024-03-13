87687cac212bb83c74a4898339051c072a59a2a8
haygcao <ngokelcho@outlook.com>
2024-03-13 14:41:45 +0800

    Update location_service.dart

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -5,46 +5,9 @@ import 'package:flutter_libphonenumber/flutter_libphonenumber.dart';
 import 'package:models/location_data.dart';
 
 class LocationService {
-  class LocationService {
-  final PhoneNumberUtil _phoneNumberUtil;
-  final PhoneNumberOfflineGeocoder _geocoder;
-  final PhoneNumberToCarrierMapper _carrierMapper;
 
-  LocationService()
-      : _phoneNumberUtil = getIt<PhoneNumberUtil>(),
-        _geocoder = getIt<PhoneNumberOfflineGeocoder>(),
-        _carrierMapper = getIt<PhoneNumberToCarrierMapper>();
-  // 获取来电号码的归属地
-  Future<LocationData> getCallerLocation(String phoneNumber) async {
-    // 解析来电号码
-    PhoneNumber parsedPhoneNumber = PhoneNumberParser.parse(phoneNumber);
-
-    // 获取来电号码的国家代码
-    String countryCode = parsedPhoneNumber.countryCode;
-
-    // 获取来电号码的归属地
-    String region = await _getRegionFromCountryCode(countryCode);
-
-    // 获取来电号码的省份
-    String province = await _getProvinceFromRegion(phoneNumber, Locale);
-
-    // 获取来电号码的运营商
-    String carrier = await _getCarrierFromPhoneNumber(phoneNumber, Locale);
-
-    // 判断来电号码是否为本地号码
-    bool isLocalNumber = await _isLocalNumber(phoneNumber);
-
-    // 返回来电号码的归属地信息
-    return LocationData(
-      region: region,
-      province: province,
-      carrier: carrier,
-      isLocalNumber: isLocalNumber,
-    );
-  }
-
-  // 从国家代码获取归属地的私有方法
-  Future<String> _getRegionFromCountryCode(String countryCode) async {
+  // 从国家代码获取国家
+  Future<String> _getCountryCodeFromCountryCode(String countryCode) async {
     // 使用 PhoneNumberUtil 库获取归属地
     PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
     try {
@@ -56,19 +19,61 @@ class LocationService {
       return null;
     }
   }
-  
-  // 从归属地获取省份的私有方法
-  Future<String> _getProvinceFromRegion(String region, Locale locale) async {
-    // 使用 PhoneNumberOfflineGeocoder 库获取省份
+
+  // 获取来电号码的归属地
+  Future<String> _getRegionFromRegion(String region, Locale locale) async {
+    // 使用 PhoneNumberOfflineGeocoder  
     PhoneNumberOfflineGeocoder geocoder = PhoneNumberOfflineGeocoder.getInstance();
     return geocoder.getDescriptionForNumber(phoneNumber, locale);
   }
 
-  // 从号码获取运营商的私有方法
+  // 从号码获取运营商
   Future<String> _getCarrierFromPhoneNumber(String phoneNumber, Locale locale) async {
     // 使用 PhoneNumberToCarrierMapper 库获取运营商
     PhoneNumberToCarrierMapper carrierMapper = PhoneNumberToCarrierMapper.getInstance();
     return carrierMapper.getNameForNumber(phoneNumber, locale);
+  }
+
+  // 获取来电号码的类型
+  Future<PhoneNumberType> getNumberType(String phoneNumber) async {
+    // 使用 PhoneNumberUtil 库获取类型
+    PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
+    return phoneNumberUtil.getNumberType(phoneNumber);
+  }
+
+  // 获取来电号码的归属地
+  Future<LocationData> getCallerLocation(String phoneNumber) async {
+    // 解析来电号码
+    PhoneNumber parsedPhoneNumber = PhoneNumberParser.parse(phoneNumber);
+
+    // 检查电话号码是否有效
+    if (!PhoneNumberUtil.isValidNumber(phoneNumber)) {
+      return null;
+    }
+
+    // 获取来电号码的类型
+    PhoneNumberType numberType = await getNumberType(phoneNumber);
+
+    // 获取来电号码的国家代码
+    String countryCode = parsedPhoneNumber.countryCode;
+
+    // 获取来电号码归属地
+    String region = await _PhoneNumberOfflineGeocoder(phoneNumber, Locale);
+
+    // 获取来电号码的运营商
+    String carrierCode = await PhoneNumberToCarrierMapper.getCodeForNumber(phoneNumber, locale);
+
+    // 判断来电号码是否为本地号码
+    bool isLocalNumber = await _isLocalNumber(phoneNumber);
+
+    // 返回来电号码的归属地信息
+    return LocationData(
+      region: region,
+      countryCode: countryCode,
+      carrier: carrier,
+      numberType: numberType,
+      isLocalNumber: isLocalNumber,
+    );
   }
 
   // 判断来电号码是否为本地号码的私有方法
@@ -87,16 +92,11 @@ class LocationService {
 // 来电号码归属地数据模型
 class LocationData {
   final String region;
-  final String province;
+  final String countryCode;
   final String carrier;
+  final PhoneNumberType numberType;
   final bool isLocalNumber;
   final String phoneNumber;
 
   LocationData({
-    required this.region,
-    required this.province,
-    required this.carrier,
-    required this.isLocalNumber,
-    required this.phoneNumber,
-  });
-}
+    required
