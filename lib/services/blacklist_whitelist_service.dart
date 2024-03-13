3c49e1271d7a1e0a90ff2c06e01e7f6ca844bdc1
haygcao <ngokelcho@outlook.com>
2024-03-13 17:08:18 +0800

    Update blacklist_whitelist_service.dart

--- lib/services/blacklist_whitelist_service.dart
+++ lib/services/blacklist_whitelist_service.dart
@@ -29,3 +29,44 @@ class WhitelistEntry {
     required this.isSubscribed,
   });
 }
+class BlacklistService {
+  final List<BlacklistEntry> _entries = [];
+
+  void add(BlacklistEntry entry) {
+    _entries.add(entry);
+  }
+
+  void remove(BlacklistEntry entry) {
+    _entries.remove(entry);
+  }
+
+  bool contains(String phoneNumber) {
+    return _entries.any((entry) => entry.phoneNumber == phoneNumber);
+  }
+
+  List<BlacklistEntry> getSubscribedEntries() {
+    return _entries.where((entry) => entry.isSubscribed).toList();
+  }
+
+}
+
+class WhitelistService {
+  final List<WhitelistEntry> _entries = [];
+
+  void add(WhitelistEntry entry) {
+    _entries.add(entry);
+  }
+
+  void remove(WhitelistEntry entry) {
+    _entries.remove(entry);
+  }
+
+  bool contains(String phoneNumber) {
+    return _entries.any((entry) => entry.phoneNumber == phoneNumber);
+  }
+
+  List<WhitelistEntry> getSubscribedEntries() {
+    return _entries.where((entry) => entry.isSubscribed).toList();
+  }
+
+}
