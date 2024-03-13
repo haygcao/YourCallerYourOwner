c06be1ea292f974b5661d0e9f17210c13896a1c2
haygcao <ngokelcho@outlook.com>
2024-03-13 16:21:33 +0800

    Update subscription_model.dart

--- lib/models/subscription_model.dart
+++ lib/models/subscription_model.dart
@@ -6,6 +6,7 @@ class SubscriptionModel {
   final String name;
   final String phoneNumber;
   final bool enabled;
+  final String url;
   final bool isWhitelist;
   final bool isBlacklist;
 
@@ -16,6 +17,7 @@ class SubscriptionModel {
     required this.enabled,
     required this.isWhitelist,
     required this.isBlacklist,
+    required this.url,
   });
 
   factory SubscriptionModel.fromJson(Map<String, dynamic> json) =>
