6bcd80e1a393d0212e004cfb663dc8afd945cba1
haygcao <ngokelcho@outlook.com>
2024-03-21 00:08:37 +0800

    Update CallerInfoOverlay.dart

--- lib/screens/CallerInfoOverlay.dart
+++ lib/screens/CallerInfoOverlay.dart
@@ -1,180 +1,361 @@
 import 'package:flutter/material.dart';
 import 'package:screens/caller_id_service.dart';
 
-import 'package:flutter/material.dart';
-import 'package:models/caller_id_data.dart';
+
 import 'package:rxdart/rxdart.dart';
 
 class CallerIdOverlay extends StatefulWidget {
-  final CallerIdData callerIdData;
+ final CallerIdData callerIdData;
 
-  const CallerIdOverlay({Key? key, required this.callerIdData}) : super(key: key);
+ const CallerIdOverlay({Key? key, required this.callerIdData}) : super(key: key);
 
-  @override
-  State<CallerIdOverlay> createState() => _CallerIdOverlayState();
+ @override
+ State<CallerIdOverlay> createState() => _CallerIdOverlayState();
 }
 
 class _CallerIdOverlayState extends State<CallerIdOverlay> {
-  final _overlayEntry = OverlayEntry();
+ final _overlayEntry = OverlayEntry();
+  
+ @override
+ void initState() {
+  super.initState();
 
-  @override
-  void initState() {
-    super.initState();
+  // 将 OverlayEntry 插入到 Overlay 中
+  _overlayEntry.insert(_getOverlay());
 
-    // 将 OverlayEntry 插入到 Overlay 中
-    _overlayEntry.insert(_getOverlay());
-
-    // 监听 callerIdData 变化并更新浮动方块
-    _callerIdSubject.listen((callerIdData) {
-      setState(() {
-        // 更新 _callerIdData 变量
-        _callerIdData = callerIdData;
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _buildCallerIdBox();
-  }
-
-  @override
-  void dispose() {
-    // 移除 OverlayEntry
+  // 监听通话状态变化
+  _callStateSubject.listen((callState) {
+   if (callState == CallState.ended) {
+    // 通话结束后关闭浮动方块
     _overlayEntry.remove();
-    super.dispose();
-  }
+   }
+  });
+ }
+ @override
+ void initState() {
+  super.initState();
 
-  Overlay _getOverlay() {
-    return Overlay.of(context) ?? Overlay.of(context.findRootAncestor());
-  }
+  // 将 OverlayEntry 插入到 Overlay 中
+  _overlayEntry.insert(_getOverlay());
 
-  Widget _buildCallerIdBox() {
-    return Dismissible(
-      key: ValueKey(_callerIdData.phoneNumber),
-      onDismissed: (_) {
-        // 关闭浮动方块
+  // 监听 callerIdData 变化并更新浮动方块
+  _callerIdSubject.listen((callerIdData) {
+   setState(() {
+    // 更新 _callerIdData 变量
+    _callerIdData = callerIdData;
+   });
+  });
+ }
+
+ @override
+ Widget build(BuildContext context) {
+  return _buildCallerIdBox();
+ }
+
+ @override
+ void dispose() {
+  // 移除 OverlayEntry
+  _overlayEntry.remove();
+  super.dispose();
+ }
+
+ Overlay _getOverlay() {
+  return Overlay.of(context) ?? Overlay.of(context.findRootAncestor());
+ }
+
+ Widget _buildCallerIdBox() {
+  return Dismissible(
+   key: ValueKey(_callerIdData.phoneNumber),
+   onDismissed: (_) {
+    // 关闭浮动方块
+    _overlayEntry.remove();
+   },
+   child: GestureDetector(
+    onPanUpdate: (details) {
+     // 更新浮动方块的位置
+     _overlayEntry.overlay.insert(_overlayEntry.remove()..widget = _buildCallerIdBox(
+      offset: details.delta,
+     ));
+    },
+    void onHorizontalDragEnd(DragEndDetails details) {
+      // 判断用户是否想要关闭浮动窗口
+      if (details.primaryVelocity > 0) {
+        // 向右滑动关闭浮动窗口
         _overlayEntry.remove();
-      },
-      child: GestureDetector(
-        onPanUpdate: (details) {
-          // 更新浮动方块的位置
-          _overlayEntry.overlay.insert(_overlayEntry.remove()..widget = _buildCallerIdBox(
-            offset: details.delta,
-          ));
-        },
+      } else if (details.primaryVelocity < 0) {
+        // 向左滑动关闭浮动窗口
+        _overlayEntry.remove();
+      } else {
+       // 点击浮动窗口外部，不关闭浮动窗口 
+      }
+    },
         child: Container(
-          margin: const EdgeInsets.all(16.0),
-          padding: const EdgeInsets.all(16.0),
+          // 设置浮动框尺寸
+          width: 324.0,
+          height: 324.0,
+          margin: const EdgeInsets.all(44.0),
+          padding: const EdgeInsets.all(14.0),
           decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8.0),
+            gradient: LinearGradient(
+              begin: Alignment(6.0, 1),
+              end: Alignment(-1, 6.0),
+              colors: [
+                Color.fromRGBO(255, 234, 188, 1),
+                Color.fromRGBO(251, 251, 251, 1)
+              ],
+            ),
+            borderRadius: BorderRadius.circular(35.0),
           ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: 22,
+                left: 14,
+                child: Container(
+                  width: 296,
+                  height: 296,
+                  child: Stack(
+                    children: <Widget>[
+                      // 添加关闭按钮
+                      Positioned(
+                        left: 23,
+                        top: 18,
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.close,
+                            color: Color.fromRGBO(255, 0, 0, 1.0),
+                          ),
+                          onPressed: () {
+                            // 关闭浮动窗口
+                            _overlayEntry.remove();
+                          },
+                        ),
+                      ),
+                    // 显示头像
+                    Positioned(
+                      left: 27.0,
+                      top: 32.0,
+                      child: ClipOval(
+                        child: Image.network(
+                          '${_callerIdData.avatar}',
+                          width: 125,
+                          height: 125,
+                          fit: BoxFit.cover, // 让图像充滿所有可用空间
+                         ),
+                        ),
+                      ),
+
+                    // // 显示号码国家
+                    Positioned(
+                      left: 158.0,
+                      width: 151.0,
+                      top: 22.0,
+                      height: 39.0,
+                      child: Text(
+                        textAlign: TextAlign.left,
+                        '${_callerIdData.countryCode}',
+                        style: const TextStyle(
+                          color: Color.fromRGBO(144, 143, 157, 1),
+                          fontFamily: 'Noto Sans SC',
+                          fontSize: 30,
+                          letterSpacing: 0.07,
+                          fontWeight: FontWeight.normal,
+                          height: 1,
+                          // 设置自动字体大小
+                          autofontSize: true,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                     
+                    // 显示运营商
+                    Positioned(
+                      left: 158.0,
+                      width: 151.0,
+                      top: 61.0,
+                      height: 34.0,
+                      child: Text(
+                        textAlign: TextAlign.left,
+                        '${_callerIdData.carrier}',
+                        style: const TextStyle(
+                          color: Color.fromRGBO(144, 143, 157, 1),
+                          fontFamily: 'Noto Sans SC',
+                          fontSize: 30,
+                          letterSpacing: 0.07,
+                          fontWeight: FontWeight.normal,
+                          height: 1,
+                          // 设置自动字体大小
+                          autofontSize: true,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                     // 显示号码类型
+                    Positioned(
+                      left: 158.0,
+                      width: 151.0,
+                      top: 96.0,
+                      height: 34.0,
+                      child: Text(
+                        textAlign: TextAlign.left,
+                        '${_callerIdData.numberType}',
+                        style: const TextStyle(
+                          color: Color.fromRGBO(144, 143, 157, 1),
+                          fontFamily: 'Noto Sans SC',
+                          fontSize: 30,
+                          letterSpacing: 0.07,
+                          fontWeight: FontWeight.normal,
+                          height: 1,
+                          // 设置自动字体大小
+                          autofontSize: true,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),          
+                     // 显示名字
+                    Positioned(
+                      left: 158.0,
+                      width: 151.0,
+                      top: 137.0,
+                      height: 20.0,
+                      child: Text(
+                        textAlign: TextAlign.left,
+                        '${_callerIdData.name}',
+                        style: const TextStyle(
+                          color: Color.fromRGBO(44, 44, 65, 1),
+                          fontFamily: 'Noto Sans SC',
+                          fontSize: 20,
+                          letterSpacing: 0.07,
+                          fontWeight: FontWeight.normal,
+                          height: 1,
+                          // 设置自动字体大小
+                          autofontSize: true,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ), 
+                     // 显示本地号码
+                    Positioned(
+                      left: 158.0,
+                      width: 162.0,
+                      top: 137.0,
+                      height: 20.0,
+                      child: Text(
+                        textAlign: TextAlign.left,
+                        '${_callerIdData.isLocalNumber}',
+                        style: const TextStyle(
+                          color: Color.fromRGBO(44, 44, 65, 1),
+                          fontFamily: 'Noto Sans SC',
+                          fontSize: 20,
+                          letterSpacing: 0.07,
+                          fontWeight: FontWeight.normal,
+                          height: 1,
+                          // 设置自动字体大小
+                          autofontSize: true,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),      
+                     // 归属地显示本地号码
+                    Positioned(
+                      left: 55.0,
+                      width: 255.0,
+                      top: 178.0,
+                      height: 47.0,
+                      child: Text(
+                        textAlign: TextAlign.left,
+                        '${_callerIdData.region}',
+                        style: const TextStyle(
+                          color: Color.fromRGBO(144, 143, 157, 1),
+                          fontFamily: 'Noto Sans SC',
+                          fontSize: 30,
+                          letterSpacing: 0.07,
+                          fontWeight: FontWeight.normal,
+                          height: 1,
+                          // 设置自动字体大小
+                          autofontSize: true,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),         
+   
+                    // 归属地图标
+                    Positioned(
+                      left: 19.0,
+                      top: 192.0,
+                      child: SizedBox(
+                        height: 21.0,
+                        child: Icon(
+                          Icons.location_on,
+                          color: Color.fromRGBO(147, 203, 128, 1.0),
+                        ),
+                      ),
+                    ),
+                     // 显示标签
+                    Positioned(
+                      left: 55.0,
+                      width: 255.0,
+                      top: 230.0,
+                      height: 40.0,
+                      child: Text(
+                        textAlign: TextAlign.left,
+                        '${_callerIdData.labels.map((label) => label.name).join(', ')}',
+                        style: const TextStyle(
+                          color: Color.fromRGBO(242, 112, 126, 1),
+                          fontFamily: 'Noto Sans SC',
+                          fontSize: 30,
+                          letterSpacing: 0.07,
+                          fontWeight: FontWeight.normal,
+                          height: 1,
+                          // 设置自动字体大小
+                          autofontSize: true,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ), 
 
  
-              // 显示号码和归属地
-              Row(
-                children: [
-                  Text(
-                    '号码：',
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                  Text(
-                    '${_callerIdData.phoneNumber}',
-                    style: const TextStyle(fontSize: 24.0),
+                    // 显示labels图片
+                    Positioned(
+                      left: 14.0,
+                      top: 238.0,
+                      child: SizedBox(
+                        height: 26.0,
+                        child: Icon(
+                          Icons.warning,
+                          color: Color.fromRGBO(242, 112, 126, 1),
+                        ),
+                      ),
+                    ),
+                    // 显示labels图片
+ 
+                     // 显示号码
+                    Positioned(
+                      left: 35.0,
+                      width: 257.0,
+                      top: 278.0,
+                      height: 40.0,
+                      child: Text(
+                        textAlign: TextAlign.left,
+                        '${_callerIdData.labels.map((label) => label.name).join(', ')}',
+                        style: const TextStyle(
+                          color: Color.fromRGBO(44, 44, 65, 1),
+                          fontFamily: 'Noto Sans SC',
+                          fontSize: 30,
+                          letterSpacing: 0.07,
+                          fontWeight: FontWeight.normal,
+                          height: 1,
+                          // 设置自动字体大小
+                          autofontSize: true,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
                     ),
                   ],
                 ),
-                const SizedBox(height: 8.0),
-                Text(
-                  '归属地：${_callerIdData.region}',
-                  style: const TextStyle(fontSize: 16.0),
-                ),
-
-              // 显示运营商和号码类型
-              const Divider(height: 16.0),
-              Row(
-                children: [
-                  Text(
-                    '运营商：',
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                  Text(
-                    '${_callerIdData.carrier}',
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                ],
               ),
-              const SizedBox(height: 8.0),
-              Row(
-                children: [
-                  Text(
-                    '号码类型：',
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                  Text(
-                    '${_callerIdData.numberType?.name}',
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                ],
-              ),
-
-              // 显示是否为本地号码
-              const Divider(height: 16.0),
-              Row(
-                children: [
-                  Text(
-                    '是否为本地号码：',
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                  Text(
-                    '${_callerIdData.isLocalNumber}',
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                ],
-              ),
-
-              // 显示标签
-              const Divider(height: 16.0),
-              const Text('标签：'),
-              const SizedBox(height: 8.0),
-              Wrap(
-                spacing: 8.0,
-                runSpacing: 8.0,
-                children: _callerIdData.labels.map((label) => Chip(
-                  label: Text(label.name),
-                )).toList(),
-              ),
-
-              // 显示联系人信息
-              const Divider(height: 16.0),
-              if (_callerIdData.name != null || _callerIdData.avatar != null)
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (_callerIdData.name != null)
-                      Text(
-                        '联系人姓名：${_callerIdData.name}',
-                        style: const TextStyle(fontSize: 16.0),
-                      ),
-                    if (_callerIdData.avatar != null)
-                      const SizedBox(height: 8.0),
-                    if (_callerIdData.avatar != null)
-                      Image.asset(
-                        _callerIdData.avatar!,
-                        width: 100.0,
-                        height: 100.0,
-                      ),
-                  ],
-                ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
+             ),
+            );
+           }
+          }
+        }
