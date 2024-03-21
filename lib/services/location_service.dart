1abaa9df7290dd32cb2e161bbcd3eb3c7f5c286d
haygcao <ngokelcho@outlook.com>
2024-03-21 21:14:37 +0800

    Update location_service.dart

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -5,7 +5,9 @@ import 'package:flutter_libphonenumber/flutter_libphonenumber.dart';
 import 'package:models/location_model.dart';
 
 class LocationService {
-
+  // 数据库操作类
+  final DatabaseHelper _databaseHelper = DatabaseHelper();
+  
   // 从国家代码获取国家
   Future<String> _getCountryCodeFromCountryCode(String countryCode) async {
     // 使用 PhoneNumberUtil 库获取归属地
@@ -66,6 +68,33 @@ class LocationService {
     // 判断来电号码是否为本地号码
     bool isLocalNumber = await _isLocalNumber(phoneNumber);
 
+    // 查询数据库，判断该号码是否已存储
+    LocationData? locationData = await _databaseHelper.getLocationDataByPhoneNumber(phoneNumber);
+
+    // 如果号码已存储，则更新数据
+    if (locationData != null) {
+      locationData.region = region;
+      locationData.countryCode = countryCode;
+      locationData.carrier = carrier;
+      locationData.numberType = numberType;
+      locationData.isLocalNumber = isLocalNumber;
+      await _databaseHelper.updateLocationData(locationData);
+    } else {
+      // 如果号码未存储，则插入新数据
+      locationData = LocationData(
+        region: region,
+        countryCode: countryCode,
+        carrier: carrier,
+        numberType: numberType,
+        isLocalNumber: isLocalNumber,
+        phoneNumber: phoneNumber,
+      );
+      await _databaseHelper.insertLocationData(locationData);
+    }
+
+    return locationData;
+  }
+  
     // 返回来电号码的归属地信息
     return LocationData(
       region: region,
@@ -99,4 +128,12 @@ class LocationData {
   final String phoneNumber;
 
   LocationData({
-    required
+      required this.region,
+      required this.countryCode,
+      required this.carrier,
+      required this.numberType,
+      required this.isLocalNumber,
+      required this.phoneNumber,
+    });
+
+}
