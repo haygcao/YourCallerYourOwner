6d6439a6440d5349d70bedb071c2a3d3f2a60bde
haygcao <ngokelcho@outlook.com>
2024-03-23 14:36:32 +0800

    Update search_bar_ui.dart

--- lib/widgets/search_bar_ui.dart
+++ lib/widgets/search_bar_ui.dart
@@ -6,29 +6,28 @@ class SearchBarUI extends StatelessWidget {
     return Container(
       height: 50,
       decoration: BoxDecoration(
-        color: Colors.red,
+        color: Colors.white,
         borderRadius: BorderRadius.circular(10),
       ),
       child: Row(
+        mainAxisSize: MainAxisSize.min, // 使 Row 尽可能小
+        mainAxisAlignment: MainAxisAlignment.start, // 将搜索图标左对齐
+        crossAxisAlignment: CrossAxisAlignment.start, // 将文本框垂直居中
         children: [
           SizedBox(width: 10),
-          Icon(Icons.search),
-          SizedBox(width: 10),
-          Expanded(
+          Container(
+            width: 300.0, // 搜索框最小宽度为 300px
+            constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.72), // 搜索框最大宽度为屏幕宽度
             child: TextField(
-              hintText: 'Search...',
-              autofocus: true,
-              obscureText: false,
-              textInputAction: TextInputAction.search,
-              keyboardType: TextInputType.text,
-              textCapitalization: TextCapitalization.sentences,
-              style: TextStyle(
-                color: Colors.white,
+              hintText: 'Input Phone Number',
+              hintStyle: TextStyle(
+                color: Colors.grey,
                 fontSize: 16,
               ),
               decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
                 border: InputBorder.none,
-                contentPadding: EdgeInsets.only(left: 10),
+                contentPadding: EdgeInsets.only(left: 10), // 调整 hintText 位置
               ),
             ),
           ),
