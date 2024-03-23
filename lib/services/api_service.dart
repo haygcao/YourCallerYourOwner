84c0ba878889ce82cdb163ec6fef4214381cf84d
haygcao <ngokelcho@outlook.com>
2024-03-23 22:49:46 +0800

    Delete lib directory

--- lib/services/api_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:android_intent/android_intent.dart';
-final intent = AndroidIntent(action: 'android.intent.action.PHONE_STATE');
-final result = await intent.startActivityForResult();
-
-if (result.resultCode == ActivityResultCode.ok) {
-  final callerId = result.data.getStringExtra('android.intent.extra.PHONE_NUMBER');
-  print('Caller ID: $callerId');
-}
-
