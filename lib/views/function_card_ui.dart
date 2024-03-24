014e011bdca40d1a9ea2824a020289c3c3ea3320
haygcao <ngokelcho@outlook.com>
2024-03-25 00:08:59 +0800

    Create function_card_ui.dart

---
+++ lib/views/function_card_ui.dart
@@ -0,0 +1,11 @@
+class CardStyle {
+  static const Color backgroundColor = Colors.white;
+  static const BorderRadius borderRadius = BorderRadius.circular(16);
+  static const EdgeInsets margin = EdgeInsets.zero;
+  static const EdgeInsets padding = EdgeInsets.all(16);
+  static const BoxShadow shadow = BoxShadow(
+    color: Colors.grey,
+    offset: Offset(0, 4),
+    blurRadius: 6,
+  );
+}
