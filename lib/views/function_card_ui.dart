e932cefc5179410a83ddda8e5ff1a58ba681c542
haygcao <ngokelcho@outlook.com>
2024-03-25 21:50:01 +0800

    Update function_card_ui.dart

--- lib/views/function_card_ui.dart
+++ lib/views/function_card_ui.dart
@@ -1,11 +1,13 @@
 class CardStyle {
-  static const Color backgroundColor = Colors.white;
-  static const BorderRadius borderRadius = BorderRadius.circular(16);
+  static const BorderRadius borderRadius = BorderRadius.circular(25);
   static const EdgeInsets margin = EdgeInsets.zero;
-  static const EdgeInsets padding = EdgeInsets.all(16);
+  static const EdgeInsets insidepadding = EdgeInsets.only(top: 16, left: 20, right: 20, bottom: 16);
+  static const EdgeInsets iconpadding = EdgeInsets.only(top: 16, left: 20);
+  static const EdgeInsets menupadding = EdgeInsets.only(top: 16, right: 20);
+  static const EdgeInsets textpadding = EdgeInsets.only(left: 20, bottom: 16);
   static const BoxShadow shadow = BoxShadow(
     color: Colors.grey,
-    offset: Offset(0, 4),
-    blurRadius: 6,
+    offset: Offset(0, 2),
+    blurRadius: 16,
   );
 }
