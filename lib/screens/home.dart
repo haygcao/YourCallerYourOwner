8bdee30599fc3baa8ce591f5e736a53d165c06c9
haygcao <ngokelcho@outlook.com>
2024-03-25 16:44:32 +0800

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -42,136 +42,127 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double sizeBoxWidth = screenWidth * 0.83;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Caller Your Owner'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.qr_code),
-            onPressed: () {},
-          ),
-        ],
-      ),
       body: Column(
         children: [
-          ClipRRect(
-            borderRadius: HomeBackgroundStyle.borderRadius,
-            child: Container(
-              color: HomeBackgroundStyle.backgroundColor,
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: HomePaddingStyle.all,
-                    child: Text(
-                      'Your Caller Your Owner',
-                      style: LogotitleTextStyle,
-                    ),
-                  ),
-                ],
-              ),
+          const Padding(
+            padding: EdgeInsets.only(top: 16, left: 16),
+            child: Text(
+              'Your Caller Your Owner',
+              style: LogotitleTextStyle,
             ),
           ),
           const Padding(
-            padding: HomePaddingStyle.horizontal,
+            padding: EdgeInsets.only(top: 16),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Flexible(
                   child: SearchBar(),
                 ),
                 const SizedBox(width: 16),
-                ScanBar(),
+                Padding(
+                  padding: EdgeInsets.only(top: 16, left: 16),
+                  child: ScanBar(),
+                ),
               ],
             ),
           ),
-          const Padding(
-            padding: HomePaddingStyle.horizontal,
-            child: CustomSwiper(),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 16, left: 16),
+              child: CustomSwiper(),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: HomeBackgroundStyle.borderRadius,
+            child: Container(
+              color: HomeBackgroundStyle.backgroundColor,
+              height: screenHeight * 0.25,
+            ),
           ),
           Container(
             color: Colors.black,
             child: Column(
               children: [
                 const Padding(
-                  padding: HomePaddingStyle.all,
+                  padding: EdgeInsets.only(top: 20, right: 8, bottom: 16, left: 8),
                   child: Text(
                     'Manage Rules',
                     style: subtitleTextStyle,
                   ),
                 ),
+                // ... other manage rules content
 
-                
                 const Padding(
-                  padding: HomePaddingStyle.horizontal,
-                   Container(
-  decoration: rejectCallsBackgroundStyle,
-  padding: rejectCallsPaddingStyle,
-  constraints: BoxConstraints(
-    minWidth: rejectCallsMinWidth,
-    maxWidth: rejectCallsMaxWidth,
-  ),
-  child: Row(
-    children: [
-      Text(
-        'Reject All Blacklist Calls (Not Advised)',
-        style: rejectCallsTextStyle,
-      ),
-      const Spacer(),
-GestureDetector(
-  onTap: () {
-    // Handle switch tap event
-  },
-  child: Switch(
-    value: false,
-    onChanged: (value) {},
-    style: switchStyle,
-    thumbIcon: thumbIcon,
-  ),
-),
-    ],
-  ),
-),
-                
+                  padding: EdgeInsets.only(top: 20, right: 8, bottom: 16, left: 8),
+                  child: Container(
+                    decoration: rejectCallsBackgroundStyle,
+                    padding: rejectCallsPaddingStyle,
+                    constraints: BoxConstraints(
+                      minWidth: rejectCallsMinWidth,
+                      maxWidth: rejectCallsMaxWidth,
+                    ),
+                    child: Row(
+                      children: [
+                        Text(
+                          'Reject All Blacklist Calls (Not Advised)',
+                          style: rejectCallsTextStyle,
+                        ),
+                        const Spacer(),
+                        GestureDetector(
+                          onTap: () {
+                            // Handle switch tap event
+                          },
+                          child: Switch(
+                            value: false,
+                            onChanged: (value) {},
+                            style: switchStyle,
+                            thumbIcon: thumbIcon,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-
-                
               ],
             ),
           ),
-Expanded(
-  child: ListView.builder(
-    itemCount: cards.length, // Use the length of the cards list
-    itemBuilder: (context, index) {
-      final card = cards[index]; // Access the card from the list
-      return GestureDetector(
-        onTap: () => Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => card.page,
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(top: 4, right: 8, bottom: 16, left: 8),
+              child: ListView.builder(
+                itemCount: cards.length,
+                itemBuilder: (context, index) {
+                  final card = cards[index];
+                  final double cardRatio = 3.1 / 2; // Use the aspect ratio of your cards
+                  final double totalWidth = MediaQuery.of(context).size.width * 0.83;
+                  final double desiredSpacing = totalWidth / (cardRatio + 1.0);
+                  final double desiredWidth = desiredSpacing * cardRatio;
+                  return GestureDetector(
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => card.page,
+                      ),
+                    ),
+                    child: SizedBox(
+                      width: desiredWidth,
+                      child: card,
+                    ),
+                  );
+                },
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                separatorBuilder: (context, index) => const SizedBox(width: 8.0),
+              ),
+            ),
           ),
-        ),
-        child: card,
-      );
-    },
-    scrollDirection: Axis.horizontal,
-    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-    separatorBuilder: (context, index) => const SizedBox(width: 16.0),
-  ),
-),
         ],
       ),
       bottomNavigationBar: const NavigationBar
     );
   }
 }
-
-
-
-
-
-
-
