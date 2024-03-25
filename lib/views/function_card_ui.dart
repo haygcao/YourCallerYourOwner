8952c6762feb2208a97c27505d8cfb8f680d46b2
haygcao <ngokelcho@outlook.com>
2024-03-25 23:02:20 +0800

    Update function_card_ui.dart

--- lib/views/function_card_ui.dart
+++ lib/views/function_card_ui.dart
@@ -6,8 +6,24 @@ class CardStyle {
   static const EdgeInsets menupadding = EdgeInsets.only(top: 16, right: 20);
   static const EdgeInsets textpadding = EdgeInsets.only(left: 20, bottom: 16);
   static const BoxShadow shadow = BoxShadow(
-    color: Colors.grey,
+    color: Color(0x0A000000),
+    spreadRadius: 0.0, // 阴影扩散半径
     offset: Offset(0, 2),
     blurRadius: 16,
+  );
+}
+class IconStyle {
+  static const Color iconColor = Colors.white; // 图标颜色
+  static const double iconSize = 24.0; // 图标尺寸
+}
+class MenuIconStyle {
+  static const Color menuIconColor = Colors.white; // 图标颜色
+  static const double menuIconSize = 24.0; // 图标尺寸
+  );
+}
+class TextStyle {
+  static const TextStyle textStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 14.0,
   );
 }
