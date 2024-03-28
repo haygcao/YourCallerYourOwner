774840897ff6452245bd9f4c9074b7406ebb8f99
haygcao <ngokelcho@outlook.com>
2024-03-28 14:44:59 +0800

    Update subscription_add.dart

--- lib/views/subscription/subscription_add.dart
+++ lib/views/subscription/subscription_add.dart
@@ -121,125 +121,122 @@ class _AddSubscriptionPageState extends State<AddSubscriptionPage> {
               SizedBox(height: 16.0),
               
 
-            // 打开本地文件夹
-            SizedBox(height: 16.0),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  child: Row(
+              // 打开本地文件夹
+              SizedBox(height: 16.0),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        // 使用 EdgeInsets.only() 设置图标按钮居左
+                        IconButton(
+                          icon: Icon(Icons.folder),
+                          onPressed: () async {
+                            // 打开本地文件夹
+                            FilePickerResult? result = await FilePicker.platform.pickFiles(
+                              type: FileType.any,
+                            );
+
+                            // 检查用户是否选择了文件
+                            if (result != null && result.files.single != null) {
+                              String filePath = result.files.single.path;
+
+                              // 根据文件类型解析数据
+
+                              if (_isCsvFile(filePath)) {
+                                importSubscriptionsFromFile(filePath);
+                              } else if (_isJsonFile(filePath)) {
+                                importSubscriptionsFromFile(filePath);
+                              } else {
+                                throw Exception('Invalid file format');
+                              }
+                            }
+                          },
+                          padding: EdgeInsets.only(left: 0.0),
+                          iconButtonStyle: openLocalFolderButtonStyle,
+                        ),
+                        Expanded(
+                          child: TextField(
+                            enabled: false,
+                            decoration: InputDecoration(
+                              labelText: '本地文件夹',
+                              style: inputTextStyle,
+                              decoration: inputBoxDecoration,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16.0),              
+              // 白名单和黑名单
+              Column(
+                children: <Widget>[
+                  Row(
                     children: <Widget>[
-                     // 使用 EdgeInsets.only() 设置图标按钮居左   
-                      IconButton(
-                        icon: Icon(Icons.folder),
-                onPressed: () async {
-                  // 打开本地文件夹
-                  FilePickerResult? result = await FilePicker.platform.pickFiles(
-                    type: FileType.any,
-                  );
-
-                  // 检查用户是否选择了文件
-                  if (result != null && result.files.single != null) {
-                    String filePath = result.files.single.path;
-
-                    // 根据文件类型解析数据
-
-                    if (_isCsvFile(filePath)) {
-                      importSubscriptionsFromFile(filePath);
-                    } else if (_isJsonFile(filePath)) {
-                      importSubscriptionsFromFile(filePath);
-                    } else {
-                      throw Exception('Invalid file format');
-                    }
-                  }
-                },
-                padding: EdgeInsets.only(left: 0.0),
-                        iconButtonStyle: ButtonStyle.openLocalFolderButtonStyle
-                      ),
                       Expanded(
-                        child: TextField(
-                          enabled: false,
-                          decoration: InputDecoration(
-                            labelText: '本地文件夹',
-                            style: inputTextStyle,
-                            // 应用 inputBoxDecoration 样式
-                            decoration: inputBoxDecoration,
+                        child: Container(
+                          width: whiteBoxSize.width,
+                          height: whiteBoxSize.height,
+                          decoration: whiteBoxDecoration,
+                          child: Row(
+                            children: <Widget>[
+                              Text(
+                                '白名单',
+                                style: whiteTextStyle,
+                              ),
+                              SizedBox(width: 8.0),
+                              SwitchListTile(
+                                value: _isWhitelist,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _isWhitelist = value;
+                                    _isBlacklist = !value;
+                                  });
+                                },
+                                style: shieldSwitchStyle, // 应用 shieldSwitchStyle
+                              ),
+                            ],
                           ),
                         ),
                       ),
                     ],
                   ),
-                ),
-              ],
-            ), 
-              
-Column(
-  children: <Widget>[
-    Row(
-      children: <Widget>[
-        Expanded(
-          child: Container(
-            width: whiteBoxSize.width,
-            height: whiteBoxSize.height,
-            decoration: whiteBoxDecoration,
-            child: Row(
-              children: <Widget>[
-                Text(
-                  '白名单',
-                  style: whiteTextStyle,
-                ),
-                SizedBox(width: 8.0),
-                Switch(
-                  value: _isWhitelist,
-                  onChanged: (value) {
-                    setState(() {
-                      _isWhitelist = value;
-                      _isBlacklist = !value;
-                    });
-                  },
-                  style: shieldSwitchStyle,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
-    ),
-
-    SizedBox(height: 16.0),
-
-    Row(
-      children: <Widget>[
-        Expanded(
-          child: Container(
-            width: whiteBoxSize.width,
-            height: whiteBoxSize.height,
-            decoration: whiteBoxDecoration,
-            child: Row(
-              children: <Widget>[
-                Text(
-                  '黑名单',
-                  style: whiteTextStyle,
-                ),
-                SizedBox(width: 8.0),
-                Switch(
-                  value: _isBlacklist,
-                  onChanged: (value) {
-                    setState(() {
-                      _isBlacklist = value;
-                      _isWhitelist = !value;
-                    });
-                  },
-                  style: shieldSwitchStyle,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
-    ),
-  ],
-),
-
+                  SizedBox(height: 16.0),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          width: whiteBoxSize.width,
+                          height: whiteBoxSize.height,
+                          decoration: whiteBoxDecoration,
+                          child: Row(
+                            children: <Widget>[
+                              Text(
+                                '黑名单',
+                                style: whiteTextStyle,
+                              ),
+                              SizedBox(width: 8.0),
+                              SwitchListTile(
+                                value: _isBlacklist,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _isBlacklist = value;
+                                    _isWhitelist = !value;
+                                  });
+                                },
+                                style: shieldSwitchStyle, // 应用 shieldSwitchStyle
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
               
               SizedBox(height: 16.0),
               ElevatedButton(
